22af0130736585acd0bb3b054a92e16c6c995613
Hanzala <bigapplestorekmr@gmail.com>
2025-04-20 01:59:29 +0530

    landing

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='flex my-6  md:justify-around justify-between  text-medium md:text-2xl font-bold md:font-extrabold md:p-4 p-8 items-center '>
       <div className='hover:text-black shadow-lg pl-0 pr-11 md:text-2xl text-xl'>
         
-       <Link to="/">CopyPasta</Link>
+       <Link to="/">Bullish</Link>
       </div>
       <div className='flex space-x-3 md:space-x-7 '>
         <div className='hover:text-black shadow-lg'><Link to="/explore">explore </Link></div>
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,9 +1,9 @@
 const LandingPage = () => {
     return (
         <div className="pl-5 flex justify-center items-center min-h-170 md:text-5xl text-2xl font-bold text-black">
-            welcome to the minimal blogging web-app. <br></br>
+        "welcome to bullish, a minimal blog-app.<br></br>
             we only care about <br></br>
-            knowledge not styling
+            knowledge not styling"
         </div>
     )
 }
